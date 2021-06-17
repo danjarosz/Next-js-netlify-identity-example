d94f268492d9c5b2599218d5978f2a32313f85ee
danjarosz <jarosz.daniel@protonmail.com>
2021-06-17 17:03:54 +0200

    feat(authContext): prepare basic

---
+++ stores/authContext.js
@@ -0,0 +1,24 @@
+import { createContext, useEffect, useState } from "react";
+import netlifyIdentity from "netlify-identity-widget";
+
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false,
+});
+
+export const AuthContextProvider = (props) => {
+  const { children } = props;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    //init netlify identity connection
+    netlifyIdentity.init();
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;

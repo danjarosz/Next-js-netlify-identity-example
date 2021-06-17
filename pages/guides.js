f6bd02d77d9d185b764b34bf8612389bd8cd7371
danjarosz <jarosz.daniel@protonmail.com>
2021-06-17 22:21:07 +0200

    feat: fech data and render

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import AuthContext from "../stores/authContext";
 import styles from "../styles/Guides.module.css";
 
 export default function Guides() {
   const { user, authReady } = useContext(AuthContext);
+  const [guides, setGuides] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (authReady) {
@@ -16,17 +18,35 @@ export default function Guides() {
         }
       )
         .then((res) => {
+          if (!res.ok) {
+            throw Error("You must be logged in to see this content!");
+          }
+
           return res.json();
         })
         .then((data) => {
-          console.log(data);
+          setError(false);
+          setGuides(data);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setGuides(null);
         });
     }
   }, [user, authReady]);
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      {!authReady && <div>Loading...</div>}
+      {error && <div className={styles.error}>{error}</div>}
+      {guides &&
+        guides.map((guide) => (
+          <div key={guide.title} className={styles.card}>
+            <h3>{guide.title}</h3>
+            <h4>Written by: {guide.author}</h4>
+            <p>Lorem...</p>
+          </div>
+        ))}
     </div>
   );
 }
